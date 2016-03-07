be644164f2cf45aa5265fe2cfea843a0956ece62
Andrew Berezovskyi <andrew@berezovskyi.me>
2016-03-07 01:50:22 +0100

    Initial commit of sample Java Lambda fn

---
+++ src/main/java/me/berezovskyi/aws/testlambda/StreamHandler.java
@@ -0,0 +1,50 @@
+package me.berezovskyi.aws.testlambda;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.commons.io.IOUtils;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class StreamHandler {
+	
+	static DynamoDB dynamoDB = new DynamoDB(Regions.EU_WEST_1);
+	
+	public static void handler(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+    	String input = IOUtils.toString(inputStream);
+    	context.getLogger().log("Input: " + input);
+    	IOUtils.write(input, outputStream);    
+    	
+    	JsonParser parser = new JsonParser();
+        JsonElement element = parser.parse(input);
+        if (element.isJsonObject()) {
+            JsonObject req = element.getAsJsonObject();
+            String operation = req.get("operation").getAsString();
+            String tableName = req.get("tableName").getAsString(); // TODO if exists
+            
+            Table table = dynamoDB.getTable(tableName);
+            
+            JsonObject payload = req.get("payload").getAsJsonObject();
+            
+            if(operation == "create") {
+            	table.putItem(Item.fromJSON(payload.toString())); //let's see how bad does it go
+            }
+            
+//            JsonArray datasets = operation.getAsJsonArray("dataset");
+//            for (int i = 0; i < datasets.size(); i++) {
+//                JsonObject dataset = datasets.get(i).getAsJsonObject();
+//                System.out.println(dataset.get("album_title").getAsString());
+//            }
+        }
+    }
+}
