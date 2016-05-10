4fa2ce6c83006d8f852ac87d11efc489a6668a81
Andrew Berezovskyi <andrew@berezovskyi.me>
2016-05-10 15:15:23 +0200

    Fixes #2

--- src/main/java/me/berezovskyi/aws/testlambda/StreamHandler.java
+++ src/main/java/me/berezovskyi/aws/testlambda/StreamHandler.java
@@ -17,27 +17,27 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class StreamHandler {
-	
+
 	static DynamoDB dynamoDB = new DynamoDB(Regions.EU_WEST_1);
-	
+
 	public static void handler(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
     	String input = IOUtils.toString(inputStream);
     	context.getLogger().log("Input: " + input);
-    	IOUtils.write(input, outputStream);    
-    	
+    	IOUtils.write(input, outputStream);
+
     	JsonParser parser = new JsonParser();
         JsonElement element = parser.parse(input);
         if (element.isJsonObject()) {
             JsonObject req = element.getAsJsonObject();
             String operation = req.get("operation").getAsString();
             String tableName = req.get("tableName").getAsString();  // TODO if exists
-            
+
             Table table = dynamoDB.getTable(tableName);
-            
+
             JsonObject payload = req.get("payload").getAsJsonObject();
-            
-            if(operation == "create") {
-            	table.putItem(Item.fromJSON(payload.toString()));  // how bad can it be? }:)
+
+            if(operation.equalsIgnoreCase("create")) {
+            	table.putItem(Item.fromJSON(payload.toString()));
             }
         }
     }
